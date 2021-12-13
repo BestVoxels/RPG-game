9757200ae949823379b3fee2146eac85a9805cc8
Thanitsak Leuangsupornpong <patsorn112233@gmail.com>
2021-12-13 16:20:08 +0700

    Action Priority either Attack or Move

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -29,40 +29,15 @@ namespace RPG.Control
 
         private void Update()
         {
-            InteractWithMovement();
-            InteractWithCombat();
+            if (InteractWithCombat()) return;
+            if (InteractWithMovement()) return;
         }
         #endregion
 
 
 
         #region --Methods-- (Custom PRIVATE)
-        private void InteractWithMovement()
-        {
-            if (Input.GetMouseButton(0))
-            {
-                MoveToCursor();
-            }
-        }
-
-        private void MoveToCursor()
-        {
-            // Draw the ray GET FIRST HIT
-            if (Physics.Raycast(GetMouseRay(), out RaycastHit hitInfo))
-            {
-                _mover.MoveTo(hitInfo.point);
-            }
-        }
-
-        private void InteractWithCombat()
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                AttackTarget();
-            }
-        }
-
-        private void AttackTarget()
+        private bool InteractWithCombat()
         {
             // Draw the ray GET ALL, WON'T GET BLOCK
             RaycastHit[] hitsInfo = Physics.RaycastAll(GetMouseRay());
@@ -72,8 +47,27 @@ namespace RPG.Control
                 CombatTarget combatTarget = each.transform.GetComponent<CombatTarget>();
                 if (combatTarget == null) continue;
 
-                _fighter.Attack();
+                if (Input.GetMouseButtonDown(0))
+                {
+                    _fighter.Attack();
+                }
+                return true;
             }
+            return false;
+        }
+
+        private bool InteractWithMovement()
+        {
+            // Draw the ray GET FIRST HIT
+            if (Physics.Raycast(GetMouseRay(), out RaycastHit hitInfo))
+            {
+                if (Input.GetMouseButton(0))
+                {
+                    _mover.MoveTo(hitInfo.point);
+                }
+                return true;
+            }
+            return false;
         }
 
         private Ray GetMouseRay() => _camera.ScreenPointToRay(Input.mousePosition); // get ray direction from camera to a screen point
