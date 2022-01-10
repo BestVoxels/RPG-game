17e957e50e7e1d589d3fa814031034eafe9aeaf0
Thanitsak Leuangsupornpong <patsorn112233@gmail.com>
2022-01-10 19:13:17 +0700

    Load Correct Portals using Its References

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -7,9 +7,21 @@ namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+        private enum DestinationIdentifier
+        {
+            A,
+            B,
+            C,
+            D,
+            E
+        }
+
+
+
         #region --Fields-- (Inspector)
         [SerializeField] private int _sceneIndexToLoad = -1;
-        [SerializeField] private Transform spawnPoint;
+        [SerializeField] private Transform _spawnPoint;
+        [SerializeField] private DestinationIdentifier _destination;
         #endregion
 
 
@@ -29,6 +41,12 @@ namespace RPG.SceneManagement
         #region --Methods-- (Custom PRIVATE)
         private IEnumerator Transition()
         {
+            if (_sceneIndexToLoad < 0)
+            {
+                Debug.LogError("Please Set SceneIndexToLoad First!");
+                yield break;
+            }
+
             DontDestroyOnLoad(gameObject);
 
             yield return SceneManager.LoadSceneAsync(_sceneIndexToLoad);
@@ -44,7 +62,7 @@ namespace RPG.SceneManagement
             Portal[] portals = FindObjectsOfType<Portal>();
 
             foreach (Portal each in portals)
-                if (each != this)
+                if (each != this && each._destination == _destination)
                     return each;
 
             return null;
@@ -53,8 +71,8 @@ namespace RPG.SceneManagement
         private void UpdatePlayer(Portal otherPortal)
         {
             GameObject player = GameObject.FindWithTag("Player");
-            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position); // No Conflict with NavMeshAgent by setting position this way
-            player.transform.rotation = otherPortal.spawnPoint.rotation;
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal._spawnPoint.position); // No Conflict with NavMeshAgent by setting position this way
+            player.transform.rotation = otherPortal._spawnPoint.rotation;
         }
         #endregion
     }
