c8e657c178c2ddca69a15d00a46133fd948e762c
Thanitsak Leuangsupornpong <patsorn112233@gmail.com>
2022-02-01 17:07:35 +0700

    Raycast Calculate NavMesh Path so can't navigate too far

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -10,7 +10,11 @@ namespace RPG.Control
     public class PlayerController : MonoBehaviour
     {
         #region --Fields-- (Inspector)
+        [Tooltip("Only Navigate where there is NavMesh with 'extended range' to place where there is No NavMesh")]
+        [Range(0.1f, 4f)]
         [SerializeField] private float _maxNavMeshDetectionRange = 2f;
+        [Tooltip("How long can Player Travel? need to have maximum otherwise player can just go in one shot on the other side of river for example.")]
+        [SerializeField] private float _maxNavMeshPathLength = 40f;
         [SerializeField] private CursorMapping[] _cursorMappings = null;
         #endregion
 
@@ -95,23 +99,6 @@ namespace RPG.Control
             return false;
         }
 
-        private bool RaycastNavMesh(out Vector3 target)
-        {
-            // Raycast to Terrain
-            if (Physics.Raycast(GetMouseRay(), out RaycastHit hitInfo))
-            {
-                // Find Nearest NavMeshPoint
-                if (NavMesh.SamplePosition(hitInfo.point, out NavMeshHit navMeshHit, _maxNavMeshDetectionRange, NavMesh.AllAreas))
-                {
-                    target = navMeshHit.position;
-                    return true;
-                }
-            }
-
-            target = Vector3.zero;
-            return false;
-        }
-
         private RaycastHit[] RaycastAllSorted()
         {
             // Draw the ray GET ALL, WON'T GET BLOCK & SORTED with Hit Distance
@@ -126,6 +113,46 @@ namespace RPG.Control
             Array.Sort(distances, hits);
 
             return hits;
+        }
+
+        private bool RaycastNavMesh(out Vector3 target)
+        {
+            // Raycast to Terrain
+            if (Physics.Raycast(GetMouseRay(), out RaycastHit hitInfo))
+            {
+                // Only Navigate where there is NavMesh
+                if (NavMesh.SamplePosition(hitInfo.point, out NavMeshHit navMeshHit, _maxNavMeshDetectionRange, NavMesh.AllAreas))
+                {
+                    target = navMeshHit.position;
+
+                    // Only Navigate where the NavMeshPath is not Cut (like NavMesh on the roof) & Not Too Far Away
+                    NavMeshPath path = new NavMeshPath();
+                    if (NavMesh.CalculatePath(transform.position, target, NavMesh.AllAreas, path))
+                    {
+                        if (path.status == NavMeshPathStatus.PathComplete && GetPathLength(path) < _maxNavMeshPathLength)
+                            return true;
+                    }
+                }
+            }
+
+            target = Vector3.zero;
+            return false;
+        }
+
+        private float GetPathLength(NavMeshPath path)
+        {
+            // SUM All Distance Between each Corners Pair that Player has to travel
+
+            Vector3[] pathCorners = path.corners;
+            float totalLength = 0f;
+            if (pathCorners.Length < 2) return totalLength;
+
+            for (int i = 1; i < pathCorners.Length; i++)
+            {
+                totalLength += Vector3.Distance(pathCorners[i - 1], pathCorners[i]);
+            }
+            
+            return totalLength;
         }
 
         private Ray GetMouseRay() => _camera.ScreenPointToRay(Input.mousePosition); // get ray direction from camera to a screen point
