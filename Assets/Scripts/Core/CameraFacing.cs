cae75f7b583eef0901767dedb0fac511ff89099d
Thanitsak Leuangsupornpong <patsorn112233@gmail.com>
2022-02-01 18:25:12 +0700

    In Game Damage Text UI

---
+++ Assets/Scripts/Core/CameraFacing.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class CameraFacing : MonoBehaviour
+    {
+        #region --Fields-- (In Class)
+        private Camera _mainCamera;
+        #endregion
+
+
+
+        #region --Methods-- (Built In)
+        private void Awake()
+        {
+            _mainCamera = Camera.main;
+        }
+
+        private void Update()
+        {
+            transform.forward = _mainCamera.transform.forward;
+
+            // OLD Code Formula to Rotation
+            //float result = _mainCamera.transform.eulerAngles.y + (-transform.parent.eulerAngles.y);
+            //transform.localEulerAngles = new Vector3(0f, result, 0f);
+        }
+        #endregion
+    }
+}
