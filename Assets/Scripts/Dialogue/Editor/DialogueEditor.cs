255c1ddab49565ab0dc8ce59c02c12a2fd1fd565
Thanitsak Leuangsupornpong <patsorn112233@gmail.com>
2022-03-01 23:09:02 +0700

    Test Drawing OnGUI

--- Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -1,11 +1,24 @@
 using UnityEditor;
 using UnityEditor.Callbacks;
+using UnityEngine;
 
 namespace RPG.Dialogue.Editor
 {
     public class DialogueEditor : EditorWindow
     {
-        #region --Methods-- (Custom PRIVATE) ~Call Through Editor~
+        #region --Methods-- (Built In)
+        private void OnGUI()
+        {
+            //EditorGUI.LabelField(new Rect(10, 10, 100, 10), "YOOOOO WASSAP!");
+            EditorGUILayout.LabelField("Apple");
+            EditorGUILayout.LabelField("Pear");
+            EditorGUILayout.LabelField("Mango");
+        }
+        #endregion
+
+
+
+        #region --Methods-- (Custom PRIVATE) ~Call Through Callback~
         [MenuItem("Window/RPG/Dialogue Window", false, 10000)]
         private static void ShowEditorWindow()
         {
