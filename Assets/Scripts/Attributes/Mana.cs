0a0d45dde2616b228851ada0edab452c522de5a2
Thanitsak Leuangsupornpong <patsorn112233@gmail.com>
2022-05-07 21:43:59 +0700

    Mana Regeneration

--- Assets/Scripts/Attributes/Mana.cs
+++ Assets/Scripts/Attributes/Mana.cs
@@ -5,6 +5,13 @@ namespace RPG.Attributes
 {
     public class Mana : MonoBehaviour
     {
+        #region --Fields-- (Inspector)
+        [Min(1f)]
+        [SerializeField] private float _regenerateSpeed = 10f;
+        #endregion
+
+
+
         #region --Events-- (Delegate as Action)
         public event Action OnManaPointsUpdated;
         #endregion
@@ -23,6 +30,11 @@ namespace RPG.Attributes
         {
             ManaPoints = MaxManaPoints;
         }
+
+        private void Update()
+        {
+            RegenerateMana();
+        }
         #endregion
 
 
@@ -36,6 +48,20 @@ namespace RPG.Attributes
             OnManaPointsUpdated?.Invoke();
 
             return true;
+        }
+        #endregion
+
+
+
+        #region --Methods-- (Custom PRIVATE)
+        private void RegenerateMana()
+        {
+            if (ManaPoints >= MaxManaPoints) return;
+
+            ManaPoints += Time.deltaTime * _regenerateSpeed;
+            ManaPoints = Mathf.Clamp(ManaPoints, 0f, MaxManaPoints);
+
+            OnManaPointsUpdated?.Invoke();
         }
         #endregion
     }
