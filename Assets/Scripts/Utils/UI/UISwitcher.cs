2607e30836f72f4cd633459c67bd2c84b38c8657
Thanitsak Leuangsupornpong <patsorn112233@gmail.com>
2022-06-06 23:08:26 +0700

    Switching UI Screens & Change ShowHideUI namespace

---
+++ Assets/Scripts/Utils/UI/UISwitcher.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace RPG.Utils.UI
+{
+    /// <summary>
+    /// Place on the GameObject that act as a Parent that contains bunch of Children GameObjects for switching on/off.
+    /// </summary>
+    public class UISwitcher : MonoBehaviour
+    {
+        #region --Fields-- (Inspector)
+        [SerializeField] private GameObject _starterGameObject;
+        #endregion
+
+
+
+        #region --Methods-- (Build In)
+        private void Start()
+        {
+            SwitchTo(_starterGameObject);
+        }
+        #endregion
+
+
+
+        #region --Methods-- (Custom PUBLIC)
+        public void SwitchTo(GameObject target)
+        {
+            if (target.transform.parent != gameObject.transform) return; // Guard check only allow for children to use
+
+            foreach (Transform child in gameObject.transform)
+                child.gameObject.SetActive(false);
+
+            target.SetActive(true);
+        }
+        #endregion
+    }
+}
