cbfc4eb7a4ddedb534857d71bf147b99cfee68e4
Thanitsak Leuangsupornpong <patsorn112233@gmail.com>
2022-08-13 22:50:57 +0700

    Fix TooltipSpawner Stuck Bug with 2nd finger trick for MOBILE

--- Assets/Scripts/Utils/UI/Tooltips/TooltipSpawner.cs
+++ Assets/Scripts/Utils/UI/Tooltips/TooltipSpawner.cs
@@ -135,13 +135,13 @@ namespace RPG.Utils.UI.Tooltips
             Vector3[] slotCorners = new Vector3[4];
             GetComponent<RectTransform>().GetWorldCorners(slotCorners); // Read 4 Corners of this Hovered UI (slot UI, Quest UI). Start Clockwise from bottom left, top left, top right, bottom right
             Rect rect = new Rect(slotCorners[0], slotCorners[2] - slotCorners[0]); // Position of minimum corner, Width and Height
-            
+
 #if UNITY_EDITOR || UNITY_STANDALONE
             // Work with mouse. (Destroy when mouse is outside UI area)
             if (rect.Contains(eventData.position)) return;
 #elif UNITY_IOS || UNITY_ANDROID
             // Work with touch. (Destory when mouse is outside UI area OR when finger is lift up)
-            if (rect.Contains(eventData.position) && Input.touchCount >= 1 && Input.GetTouch(0).phase != TouchPhase.Ended) return;
+            if (rect.Contains(eventData.position) && Input.touchCount >= 1 && Input.GetTouch(Input.touchCount - 1).phase != TouchPhase.Ended) return;
 #else
             if (rect.Contains(eventData.position)) return;
 #endif
